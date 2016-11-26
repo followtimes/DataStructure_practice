9dce8100969132f8d3e4421369c113ed7baefce0
followtimes <398469327@qq.com>
2016-11-26 15:19:04 +0800

    fix merge sort

--- merge_sort.py
+++ merge_sort.py
@@ -6,22 +6,22 @@ def merge(a, low, mid, high):
     b = []
     i = low 
     j = mid + 1 
-    while i < mid +1 and j <= high:
+    while i <= mid  and j <= high:
         if a[i] <= a[j]:
             b.append(a[i])
             i += 1
         else:
             b.append(a[j])
             j += 1
-    while i < mid+1:
+    while i <= mid:
         b.append(a[i])
         i += 1
     while j <= high:
         b.append(a[j])
         j += 1
-    print b
     for i in range(0,len(b)):
-        a[i] = b[i]
+        a[low] = b[i]
+        low += 1
 
 
 def merge_sort(a, left, right):
@@ -32,13 +32,10 @@ def merge_sort(a, left, right):
     merge_sort(a, mid+1, right)
     merge(a, left, mid, right)
 
-
 if __name__ == "__main__":
     a = [11, 2, 32, 76, 27, 53, 49]
-    #a = [1, 3,5,7,2,4,8]
     print "before merge"
     print a
     merge_sort(a, 0, len(a)-1)
-    #merge(a, 0, (len(a)-1)/2, len(a)-1)
     print "after merge"
     print a
