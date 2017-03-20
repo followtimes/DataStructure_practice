089e2bd8ce683e8f1aba4fbd9ff1a7924cb69e98
followtimes <398469327@qq.com>
2017-03-20 21:52:32 +0800

    add explain to quick sort

--- quick_sorted.py
+++ quick_sorted.py
@@ -7,14 +7,19 @@ def quick_sort(a, left, right):
         low = left
         high = right
         while low < high:
+            #因为基准数为left，所以先从高处遍历，拿出比基准数小的，放在基准数的左边
             while low < high and a[high] >= key:
                 high -= 1
             a[low] = a[high]
+            #从低处开始，拿到比基准数大的，放在基准数右边
             while low < high and a[low] < key:
                 low += 1
             a[high] = a[low]
+        #将基准数放在当前基准位置
         a[low] = key
+        #基准数左边的进行上述操作
         quick_sort(a, left, low - 1)
+        #基准数右边的进行上述操作
         quick_sort(a, low + 1, right)
 
 
