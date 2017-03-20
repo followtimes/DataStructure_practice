ea5f82920433a5a1ee36a5416a31bd81a0f173d8
followtimes <398469327@qq.com>
2017-03-20 20:23:13 +0800

    add explain

--- binary_find.py
+++ binary_find.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 #--*-- coding:utf-8 --*--
 
+#取中间数，比较关键字，根据大小改变左右下标，递归再次比较关键字和中间数的大小，直至找到或者找完数组
+
 def binary_find(a, left, right, num):
     if left <= right:
         mid = (left + right)/2
