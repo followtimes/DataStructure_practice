baaa30f21e9ec2601361355920f53459da050197
zhuhenghui <zhuhenghui@xiaomi.com>
2017-03-20 09:58:10 +0800

    fix bubble sort and add simple select sort

--- simple_select.py
+++ simple_select.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python
 #--*-- coding:utf-8 --*--
 
-def bubble_sort(a):
-    for i in range(len(a) - 1):
+def simple_select(a):
+    for i in range(len(a)):
+        tmp = a[i]
         for j in range(i+1, len(a)):
-            if a[i] < a[j]:
-                (a[i], a[j]) = (a[j], a[i])
-                i += 1
+            if a[j] < tmp:
+                (a[j], tmp) = (tmp, a[j])
+        a[i] = tmp
 
 if __name__ == "__main__":
     a = [12, 21, 1, 42, 14, 42, 56,21]
     print "before sort:"
     print a
-    bubble_sort(a)
+    simple_select(a)
     print "after sort:"
     print a
 
